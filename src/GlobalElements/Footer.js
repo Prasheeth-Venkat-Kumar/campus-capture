1167bfc6ad3ce03aeeb5a7db02a45f1e8d77ebb3
nwill111 <williamsnathan2001@gmail.com>
2023-01-23 18:41:38 -0500

    Added Header + Footer, Global CSS

---
+++ src/GlobalElements/Footer.js
@@ -0,0 +1,15 @@
+import "./Footer.css"
+
+function Footer() {
+  return (
+    <div className="page-footer">
+      <p>
+        <i>Created by Nathan Williams and Prasheeth Venkat</i>
+        <br></br>
+        <i> A 100 Loops Project</i>
+      </p>
+    </div>
+  )
+}
+
+export default Footer
