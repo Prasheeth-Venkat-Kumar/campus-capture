0d69f0e9750f452394e33fe90d8dcdbac459de41
Prasheeth <55602761+Prasheeth-Venkat-Kumar@users.noreply.github.com>
2023-01-25 17:27:11 -0500

    added basic swiper slider

--- src/DisplayPage/DisplayPage.js
+++ src/DisplayPage/DisplayPage.js
@@ -1,4 +1,9 @@
 import "./DisplayPage.css"
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css"
+import "swiper/css/navigation"
+// import required modules
+import { Navigation } from "swiper"
 
 function DisplayPage() {
   return (
@@ -15,6 +20,15 @@ function DisplayPage() {
           src="./campus-capture-logo.png"
           alt="Campus Capture Logo"
         />
+      </div>
+      <div className="slide-show">
+        <Swiper navigation={true} modules={[Navigation]}>
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+        </Swiper>
       </div>
 
       <div className="page-footer">
