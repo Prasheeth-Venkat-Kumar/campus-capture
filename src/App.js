50b4b19cd1fc9814fa047a69680853df04093066
nwill111 <williamsnathan2001@gmail.com>
2023-01-27 17:11:15 -0500

    Begin Testing Onnx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import "./App.css"
-import DisplayPage from "./DisplayPage/DisplayPage"
+import "./App.css";
+import DisplayPage from "./DisplayPage/DisplayPage";
+import * as ort from "onnxruntime-web";
 
 function App() {
+  loadModel();
   return (
     <div className="App">
       <DisplayPage />
     </div>
-  )
+  );
 }
 
-export default App
+async function loadModel() {
+  console.log("Session created");
+  const session = await new ort.InferenceSession("./model.onnx");
+
+  console.log("Model loaded");
+  const input = new ort.Tensor(
+    "float32",
+    new Float32Array(224 * 224 * 3),
+    [1, 3, 224, 224]
+  );
+  const outputMap = await session.run(input);
+  const outputTensor = outputMap.values().next().value;
+  console.log("Output tensor: ${outputTensor.data}");
+}
+
+export default App;
