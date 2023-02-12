3f42e17c777917e391d98656de759de5512ffd97
Prasheeth <55602761+Prasheeth-Venkat-Kumar@users.noreply.github.com>
2023-02-11 21:34:33 -0500

    added basic paragraphs for accordion

--- src/DisplayPage/DisplayPage.js
+++ src/DisplayPage/DisplayPage.js
@@ -187,46 +187,131 @@ function DisplayPage({ predictImage, theme }) {
         <ThemeProvider theme={theme}>
           <Accordion>
             <AccordionSummary>
-              <p>How is the data collected and processed?</p>
+              <p>Problem Motivation</p>
             </AccordionSummary>
             <AccordionDetails>
               <p>
-                The data that drives the heatmap and other aspects of Campus
-                Corner is derived from the University&apos;s Wi-Fi Log Data. A
-                data processing algorithm is used to convert the raw Wi-Fi log
-                data into data that is meaningful in terms of analyzing the
-                traffic of the respective University buildings. The heatmap
-                provides an easily digestible visual breakdown of the traffic
-                patterns.
+                The purpose of this project is to create a model that can
+                identify the building shown in an image taken by a user on the
+                UNCC campus. The reason behind this is that there's currently no
+                dataset available that has images of all the buildings on the
+                UNCC campus. This project is not only fun, but it's also useful
+                as the model, if accurate, could be used on other campuses or in
+                large cities where multiple buildings need to be recognized. The
+                project is limited to the UNCC campus, but the possibilities for
+                its use in the future are endless. Imagine being able to use it
+                for self-guided tours or even navigation. The best part? We get
+                to create the dataset ourselves, making the project even more
+                meaningful to the UNCC community.
               </p>
             </AccordionDetails>
           </Accordion>
           <Accordion>
             <AccordionSummary>
-              <p> How accurate is the data and heatmap?</p>
+              <p>Dataset overview</p>
             </AccordionSummary>
             <AccordionDetails>
+              <ul className="resnet50-bullets">
+                <li>
+                  Over 2700 images, taken during day and night, covering 12
+                  buildings across campus
+                </li>
+                <li>Mainly used significant buildings (UREC/Atkins/Etc…)</li>
+                <li>
+                  Many angles, different lighting scenarios, day/night images
+                </li>
+                <li>Annotated</li>
+                <li>
+                  # of samples in training/test split: 2176 / 544 (80% / 20%)
+                </li>
+              </ul>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary>
+              <p>Why use a Convolutional Neural Network?</p>
+            </AccordionSummary>
+            <AccordionDetails>
+              {/* create a list of bullet points */}
               <p>
-                Due to limitations in the data provided, we were only able to
-                utilize one month of Wi-Fi logs. Despite this, we found that by
-                observing traffic patterns on campus, the data can be
-                generalized to any normal day (non-holiday/ break days)
-                throughout the school year.
+                {" "}
+                The research involved in building recognition is often complex
+                and requires a relatively small dataset. This project considered
+                two options for building a model: handcrafted features and
+                neural networks. The team evaluated the possibility of building
+                a Convolutional Neural Network (CNN) from scratch or using a
+                pre-trained model. One reference cited was "Building recognition
+                system based on deep learning" by P. Bezak in 2016. The team
+                experimented with different pre-trained models, data
+                augmentation techniques, and vision transformers. The solution
+                ultimately chosen was a pre-trained ResNet50 model, with the
+                last layer trained and tuned for the specific task. The
+                hyperparameters used included a learning rate of 0.0001, 15
+                epochs, an image size of (224 x 224), a horizontal flip data
+                augmentation technique, and data normalization that was
+                zero-centered. The loss function used was cross-entropy, and the
+                problem was treated as a multiclass image classification task.
+                The Adam optimizer was chosen as it is computationally efficient
+                and better suited for non-linear applications than the
+                alternative, Stochastic Gradient Descent (SGD).
               </p>
             </AccordionDetails>
           </Accordion>
           <Accordion>
             <AccordionSummary>
-              <p>What is Live Busy Rating?</p>
+              <p>Results and Oberservations</p>
             </AccordionSummary>
             <AccordionDetails>
               <p>
-                The Live Busy Rating is a prediction of the current busyness
-                given the current time and date for the respective building. The
-                Live Busy Rating is generated by machine learning that is
-                modeled on the traffic log data. Due to the data being limited
-                and extrapolated, we utilize the machine learning model to
-                provide more accurate predictions.
+                The team conducted several experiments to evaluate the
+                performance of different models for building recognition on the
+                UNCC campus. In Experiment 1, a pre-trained GoogleNet model was
+                used and achieved an accuracy of ~75% on the test set.
+                Experiment 2 used a pre-trained VGG16 model, which achieved an
+                accuracy of ~67.5%. Experiment 3 used a pre-trained ResNet50
+                with vision transforming and self-attention, but this approach
+                resulted in an accuracy of only ~50%. The image size was reduced
+                to (96 x 96) in order to reduce CUDA memory needs and a learning
+                rate of 0.00005 was used, as it produced better accuracy. In
+                Experiment 4, a CNN model was built from scratch, but it
+                produced an accuracy of only ~50% on the (64 x 64) image size
+                used. Experiment 5 used a pre-trained ResNet50 model without
+                data augmentation or normalization and achieved an accuracy of
+                ~85% on the test set. The final model chosen was the pre-trained
+                ResNet50 with data augmentation, which achieved an accuracy of
+                ~85%. To improve accuracy, the team suggests collecting more
+                diverse images with a better data collection approach, covering
+                more buildings, and increasing the number of images used. The
+                results showed that buildings with unique features had better
+                classification and that CNN models are extremely powerful and
+                perform well with limited data. The team also observed that
+                similar buildings often get confused and that a better
+                understanding of the AI "black box" is needed.
+              </p>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary>
+              <p>
+                How does this website support live client-side machine learning?
+              </p>
+            </AccordionSummary>
+            <AccordionDetails>
+              <p>
+                In this website, the ONNX WebRuntime was utilized to provide a
+                client-side solution for live image classification. The
+                pre-trained ResNet50 model was exported to the ONNX format and
+                then run in the web browser using ONNX WebRuntime. This allowed
+                for real-time image classification directly on the client-side,
+                eliminating the need for server resources and providing faster
+                and more responsive results. The support for ONNX models in ONNX
+                WebRuntime also meant that the team was able to use the
+                pre-trained ResNet50 model without having to retrain it, saving
+                valuable time and resources. Overall, the use of ONNX WebRuntime
+                was instrumental in achieving the goal of accurately predicting
+                the building in an image taken on the UNCC campus and provided a
+                practical solution for live image classification in web
+                applications.
               </p>
             </AccordionDetails>
           </Accordion>
